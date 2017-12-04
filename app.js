879a213567953336eb692b9d6ad5bdc0a172b00d
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2017-12-04 09:29:44 -0700

    adding testing file and empty app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function reverseMe(str){
+
+}
+
+
+
+module.exports = {
+  //reverseMe
+}
--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-const app = 
+const expect = require('chai').expect
+const myFunc = require('./app.js')
+
+var string = "This is a string."
+var reversed = ".gnirts a si sihT"
+
+var numberStr = "2656839"
+var numsReversed = "9386562"
+
+describe('#reverses a string', function(){
+  it('takes in a string and returns it, reversed', function(){
+    expect(myFunc.reverseMe(string).to.deep.equal(reversed))
+  })
+  it('takes a string of numbers and reverses them', function(){
+    expect(myFunc.reverseMe(numberStr).to.deep.equal(numsReversed))
+  })
+})
